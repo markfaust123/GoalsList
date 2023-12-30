5fb0090c34de519ffda2d94976fb4371d59b6f87
Mark Faust <markfaust123@gmail.com>
2023-12-29 19:56:39 -0500

    added GoalItem component with styling

--- App.tsx
+++ App.tsx
@@ -1,14 +1,41 @@
+import { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import GoalItem from "./src/components/goal-item";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState<string>("");
+  const [courseGoals, setCourseGoals] = useState<string[]>([]);
+
+  function handleGoalInput(enteredText: string) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function handleAddGoal() {
+    console.log(enteredGoalText);
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      enteredGoalText,
+    ]);
+  }
+
+  // useEffect(() => {
+
+  // }, [courseGoals])
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your course goal!"/>
-        <Button title="Add Goal" />
+        <TextInput
+          onChangeText={handleGoalInput}
+          style={styles.textInput}
+          placeholder="Your course goal!"
+        />
+        <Button onPress={handleAddGoal} title="Add Goal" />
       </View>
       <View style={styles.goalsContainer}>
-        <Text>List of goals...</Text>
+        {courseGoals.map((goal) => (
+          <GoalItem key={goal} content={goal} />
+        ))}
       </View>
     </View>
   );
